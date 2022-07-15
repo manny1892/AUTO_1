d8047d053067ecb36a45e39c1d4cac3c0b2eddec
manny1892 <manny1892@gmail.com>
2022-07-15 18:04:58 +0300

    junit4 commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,36 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldReturnOneCashback() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 1;
+        int actual = service.remain(999);
+        assertEquals(expected,actual);;
+    }
+
+    @Test
+    public void shouldReturnZeroCashback() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 0;
+        int actual = service.remain(1000);
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void shouldReturnCashback() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 999;
+        int actual = service.remain(1001);
+        assertEquals(expected,actual);;
+    }
+
+}
