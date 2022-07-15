244c38fa2645113c24791076882361a29cf51540
manny1892 <manny1892@gmail.com>
2022-07-15 17:55:29 +0300

    testng commit #2

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,36 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
 import static org.testng.Assert.*;
+
 public class CashbackHackServiceTest {
-    
+
+    @Test
+    public void shouldReturnOneCashback() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 1;
+        int actual = service.remain(999);
+        assertEquals(actual, expected, "TEST 1");
+    }
+
+    @Test
+    public void shouldReturnZeroCashback() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 0;
+        int actual = service.remain(1000);
+        assertEquals(actual, expected, "TEST 2");
+    }
+
+    @Test
+    public void shouldReturnCashback() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 999;
+        int actual = service.remain(1001);
+        assertEquals(actual, expected, "TEST 3");
+    }
+
 }
