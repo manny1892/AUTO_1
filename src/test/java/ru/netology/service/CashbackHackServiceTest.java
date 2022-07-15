9080b4244909acd314424d72e4a7de2fe65c2d21
manny1892 <manny1892@gmail.com>
2022-07-15 18:49:58 +0300

    junit4-platform commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -12,7 +12,7 @@ public class CashbackHackServiceTest {
 
         int expected = 1;
         int actual = service.remain(999);
-        assertEquals(expected,actual);;
+        assertEquals(expected,actual);
     }
 
     @Test
@@ -30,7 +30,36 @@ public class CashbackHackServiceTest {
 
         int expected = 999;
         int actual = service.remain(1001);
-        assertEquals(expected,actual);;
+        assertEquals(expected,actual);
     }
+
+    @org.junit.jupiter.api.Test
+    public void shouldReturnOneCashbackJupiter() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 1;
+        int actual = service.remain(999);
+        assertEquals(expected,actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldReturnZeroCashbackJupiter() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 0;
+        int actual = service.remain(1000);
+        assertEquals(expected,actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldReturnCashbackJupiter() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 999;
+        int actual = service.remain(1001);
+        assertEquals(expected,actual);
+    }
+
+
 
 }
